6ef53951e77893146820c41cfef212ad52a4c651
bahaa1985 <bahaa_salaheldin@live.com>
2024-02-21 01:44:03 +0200

    validate host

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import https from 'https';
 
 const app=express();
 
-app.get('/',(req,res)=>{
+
+
+
+app.get('/',func,(req,res)=>{
+
     res.write('<h1>Welcome!</h1>')
+
 })
 
 app.listen(5000,()=>{
--- shorten.js
+++ shorten.js
@@ -1,7 +1,27 @@
 import express from 'express';
+import https from 'https';
 
 const shortenrouter=express.Router();
 
-function CheckUrl(url){
-    
+async function validateHost(req,res,next){
+    var options = {
+        hostname: 'github.com',
+        port: 443,
+        path: '/',
+        method: 'GET',
+        rejectUnauthorized: true
+      };
+      
+    https.request(options,(result)=>{
+        console.log("statusCode: ", result.statusCode);
+        console.log("headers: ", result.headers);
+    })
+    .on('error',(e)=>{
+        console.error(e)
+    })
+    .end();
+
+    next();
 }
+
+
